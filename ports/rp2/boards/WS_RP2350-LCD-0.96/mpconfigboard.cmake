a9d30df4cffe8db6ac5cbb1c05336bf2bdd1ae48
Antonin ENFRUN <antonin.e@me.com>
2025-08-16 22:23:18 +0200

    ws RP2350 LCD 0.96

---
+++ ports/rp2/boards/WS_RP2350-LCD-0.96/mpconfigboard.cmake
@@ -0,0 +1,5 @@
+# cmake file for WAVESHARE RP2350-LCD-0.96
+set(PICO_BOARD "waveshare_rp2350_lcd_0.96")
+
+# To change the gpio count for QFN-80
+# set(PICO_NUM_GPIOS 48)
---
+++ ports/rp2/boards/WS_RP2350-LCD-0.96/mpconfigboard.h
@@ -0,0 +1,4 @@
+// Board and hardware specific configuration
+#define MICROPY_HW_BOARD_NAME                   "Waveshare RP2350-LCD-0.96"
+#define MICROPY_HW_FLASH_STORAGE_BYTES          (PICO_FLASH_SIZE_BYTES - 1024 * 1024)
+#define MICROPY_HW_ROMFS_BYTES                  (512 * 1024)
