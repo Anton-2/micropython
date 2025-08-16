60b136c821145a2b9d0e5e65d6466a2fa094b1a1
Antonin ENFRUN <antonin.e@me.com>
2025-08-16 22:25:07 +0200

    AE BOARDS

--- ports/rp2/boards/RPI_PICO/mpconfigboard.h
+++ ports/rp2/boards/RPI_PICO/mpconfigboard.h
@@ -1,3 +1,4 @@
 // Board and hardware specific configuration
 #define MICROPY_HW_BOARD_NAME                   "Raspberry Pi Pico"
-#define MICROPY_HW_FLASH_STORAGE_BYTES          (1408 * 1024)
+#define MICROPY_HW_FLASH_STORAGE_BYTES          (512 * 1024)
+#define MICROPY_HW_ROMFS_BYTES                  (768 * 1024)
