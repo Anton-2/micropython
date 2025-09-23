843849adf58251a5ea121f9acbf871e8d12494b9
Antonin ENFRUN <antonin.e@me.com>
2025-09-23 11:53:56 +0200

    AE_PICO2 board without network

--- ports/rp2/boards/AE_PICO2/mpconfigboard.cmake
+++ ports/rp2/boards/AE_PICO2/mpconfigboard.cmake
@@ -1,17 +1,5 @@
-# cmake file for Raspberry Pi Pico 2 W
-
-set(PICO_BOARD "pico2_w")
+# cmake file for Raspberry Pi Pico2
+set(PICO_BOARD "pico2")
 
 # To change the gpio count for QFN-80
 # set(PICO_NUM_GPIOS 48)
-
-set(MICROPY_PY_LWIP ON)
-set(MICROPY_PY_NETWORK_CYW43 ON)
-
-# Bluetooth
-set(MICROPY_PY_BLUETOOTH ON)
-set(MICROPY_BLUETOOTH_BTSTACK ON)
-set(MICROPY_PY_BLUETOOTH_CYW43 ON)
-
-# Board specific version of the frozen manifest
-set(MICROPY_FROZEN_MANIFEST ${MICROPY_BOARD_DIR}/manifest.py)
--- ports/rp2/boards/AE_PICO2/mpconfigboard.h
+++ ports/rp2/boards/AE_PICO2/mpconfigboard.h
@@ -1,24 +1,4 @@
 // Board and hardware specific configuration
-#define MICROPY_HW_BOARD_NAME                   "Raspberry Pi Pico 2 W ae"
-#define MICROPY_HW_ROMFS_BYTES                  (768 * 1024)
-#define MICROPY_HW_FLASH_STORAGE_BYTES          (PICO_FLASH_SIZE_BYTES - (1536*1024) -MICROPY_HW_ROMFS_BYTES)
-
-
-// Enable networking.
-#define MICROPY_PY_NETWORK 1
-#define MICROPY_PY_NETWORK_HOSTNAME_DEFAULT     "Pico2W"
-
-// CYW43 driver configuration.
-#define CYW43_USE_SPI (1)
-#define CYW43_LWIP (1)
-#define CYW43_GPIO (1)
-#define CYW43_SPI_PIO (1)
-
-// For debugging mbedtls - also set
-// Debug level (0-4) 1=warning, 2=info, 3=debug, 4=verbose
-// #define MODUSSL_MBEDTLS_DEBUG_LEVEL 1
-
-#define MICROPY_HW_PIN_EXT_COUNT    CYW43_WL_GPIO_COUNT
-
-int mp_hal_is_pin_reserved(int n);
-#define MICROPY_HW_PIN_RESERVED(i) mp_hal_is_pin_reserved(i)
+#define MICROPY_HW_BOARD_NAME                   "AE Pico2"
+#define MICROPY_HW_ROMFS_BYTES                  (1024 * 1024)
+#define MICROPY_HW_FLASH_STORAGE_BYTES          (PICO_FLASH_SIZE_BYTES - (1024*1024) -MICROPY_HW_ROMFS_BYTES)
